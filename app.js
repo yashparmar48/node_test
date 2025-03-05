d3ccfdb60e8f2668f13266e806e215882a41a93f
Yash Parmar <yash.parmar@excellentwebworld.in>
2025-03-05 16:23:53 +0530

    db changes

--- app.js
+++ app.js
@@ -2,6 +2,10 @@ import express from 'express'
 // const mongoose = require('mongoose');
 import cors from 'cors';
 import customerRoutes from './routes/customerRoutes.js';
+import dotenv from 'dotenv';
+dotenv.config()
+
+console.log('DATABASE_NAME',process.env.DATABASE_NAME);
 
 // console.log('customerroutes',customerRoutes);
 
