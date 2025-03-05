ba42ef33036c9a6e3dca7a978e90f65ff30bd757
Yash Parmar <yash.parmar@excellentwebworld.in>
2025-03-05 16:18:57 +0530

    changes

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ import customerRoutes from './routes/customerRoutes.js';
 
 
 const app = express();
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 
 app.use(cors());
 app.use(express.json());
